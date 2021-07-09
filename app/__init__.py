0e326ab9b77f649ea1e0143ccf062d5f86be11bf
Lint Action <lint-action@samuelmeuli.com>
2021-07-09 20:06:45 +0000

    Fix code style issues with Black

--- app/__init__.py
+++ app/__init__.py
@@ -3,25 +3,29 @@ from flask import Flask, render_template, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 # from . import db
 # from app.db import get_db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
-    user=os.getenv('POSTGRES_USER'),
-    passwd=os.getenv('POSTGRES_PASSWORD'),
-    host=os.getenv('POSTGRES_HOST'),
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
     port=5432,
-    table=os.getenv('POSTGRES_DB'))
+    table=os.getenv("POSTGRES_DB"),
+)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     username = db.Column(db.String(), primary_key=True)
     password = db.Column(db.String())
@@ -34,28 +38,30 @@ class UserModel(db.Model):
         return f"<User {self.username}>"
 
 
-#links ending in ("/") will redirect to the file within the ()
-#The same style applies to the following methods
-#This allows multiple pages to interact through links.
+# links ending in ("/") will redirect to the file within the ()
+# The same style applies to the following methods
+# This allows multiple pages to interact through links.
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/health', methods=['GET'])
+
+@app.route("/health", methods=["GET"])
 def health():
     return "Works"
 
-@app.route('/register', methods=['GET','POST'])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
+            error = "Password is required."
         elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
@@ -68,20 +74,21 @@ def register():
             return error, 418
 
     ## Return a register page
-    return render_template('register.html')
+    return render_template("register.html")
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
         user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
-            error = 'Incorrect username.'
+            error = "Incorrect username."
         elif not check_password_hash(user.password, password):
-            error = 'Incorrect password.'
+            error = "Incorrect password."
 
         if error is None:
             return "Login Successful", 200
@@ -89,19 +96,23 @@ def login():
             return error, 418
 
     ## Return a login page
-    return render_template('login.html')
+    return render_template("login.html")
+
 
 @app.route("/mamnuya")
 def Rinki():
-        return render_template('Rinki.html')
+    return render_template("Rinki.html")
+
 
 @app.route("/nandhini")
 def Nandhini():
-        return render_template('Webp.html')
+    return render_template("Webp.html")
+
 
 @app.route("/ixchel")
 def Marcela():
-        return render_template('ixchel.html')
+    return render_template("ixchel.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
